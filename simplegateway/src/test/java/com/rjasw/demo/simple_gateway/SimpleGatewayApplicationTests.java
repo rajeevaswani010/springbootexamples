79a8d82c826b66dc52365a60d71ab6a0a89c6378
rajeev <rajeev@itscoman.com>
2025-04-08 12:51:12 +0400

    gateway example + crud app

---
+++ simplegateway/src/test/java/com/rjasw/demo/simple_gateway/SimpleGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rjasw.demo.simple_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
